36b4a74b2ea0abda80662b603d6adb7bd4d66063
Boxuan Shan <boxuan.shan@gmail.com>
2024-12-06 13:22:21 -0800

    +HeapUtil, +Main

--- HeapLab/HeapUtil.java
+++ HeapLab/HeapUtil.java
@@ -5,7 +5,7 @@ import java.util.*;
  * @version 12022024
  */
 public class HeapUtil {
-  private int heapSize;
+  private int heapSize = 0;
 
   /**
    * constructor for HeapUtil
@@ -18,17 +18,17 @@ public class HeapUtil {
   /**
    * default constructor for HeapUtil
    */
-  public HeapUtil() {
-    this(0);
-  }
+  //public HeapUtil() {
+  //  this(0);
+  //}
 
   /**
    * gets the size of the heap
    * @return size of heap
    */
-  public int getHeapSize() {
-    return this.heapSize;
-  }
+  //public int getHeapSize() {
+  //  return this.heapSize;
+  //}
 
   private void swp(Comparable[] h, int a, int b) {
     Comparable t = h[a];
@@ -42,25 +42,18 @@ public class HeapUtil {
    * @param index index of heap array to start at
    */
   public void heapify(Comparable[] h, int n) {
-    int l = 2*n;
-    int r = 2*n+1;
-    //int cl =  // > 0 
-    //int cr = h[r].compareTo(h[n]); // > 0
-
-    if (n > heapSize) {
-      return;
+    if (heapSize >= 2*n && h[2*n] != null) {
+      if (h[2*n].compareTo(h[n]) < 0) {
+        swp(h, 2*n, n);
+        heapify(h, 2*n);
+      }
     }
-
-    if (l < heapSize && h[l].compareTo(h[n]) < 0) {
-      swp(h, l, n);
+    if (heapSize >= (2*n+1) && h[2*n] != null) {
+      if (h[2*n+1].compareTo(h[n]) < 0) {
+        swp(h, 2*n+1, n);
+        heapify(h, 2*n+1);
+      }
     }
-
-    if (r < heapSize && h[r].compareTo(h[n]) < 0) {
-      swp(h, l, n);
-    }
-
-    heapify(h, l);
-    heapify(h, r);
   }
   
 
@@ -68,18 +61,41 @@ public class HeapUtil {
    * rearranges a given array to satisfy the conditions for a heap
    * @param heap heap to rearrange
    */
-  //public void buildHeap(Comparable[] heap) {
-
-  //}
+  public void buildHeap(Comparable[] h) {
+    for (int i = heapSize/2; i > 0; i--) {
+      heapify(h, i);
+    }
+  }
 
   /**
    * removes and returns the root value of a given heap
    * @param heap heap to remove from
    * @return value of root 
    */
-  //public Comparable remove(Comparable[] heap) {
+  public Comparable remove(Comparable[] h) {
+    Comparable ret = h[1];
+    h[1] = h[heapSize];
+    h[heapSize] = null;
+    heapSize--;
+    heapify(h, 1);
+    return ret;
+  }
 
-  //}
+
+  /**
+   * heapifies a given heap, sifting up
+   * @param heap heap to heapify 
+   * @param index index of heap array to start at
+   */
+  public void heapifyUp(Comparable[] h, int n) {
+    int p = (n - 1) / 2;
+    if (h[n].compareTo(h[p]) > 0) { 
+      swp(h, n, p); 
+      heapifyUp(h, p); 
+    } 
+  }
+  
+
 
   /**
    * inserts an item into heap, keeping the heap property
@@ -87,17 +103,35 @@ public class HeapUtil {
    * @param item item to insert
    * @return heap in the form of an array
    */
-  //public Comparable[] insert(Comparable[] heap, Comparable item) {
+  public Comparable[] insert(Comparable[] h, Comparable i) {
+    heapSize++;
+    if (heapSize >= h.length) {
+      Comparable[] hnew = new Comparable[h.length*2];
+      for (int i = 0; i < h.length; i++) {
+        hnew[i] = h[i];
+      }
+      h = hnew;
+    }
+    h[heapSize] = i;
+    //heapifyUp(h, heapSize);
+    buildHeap(h);
+    return h;
+  }
 
-  //}
 
+  /**
+   * sorts an array using heap methods
+   * @param heap heap array to sort
+   * @return sorted array
+   */
+  public Comparable[] heapSort(Comparable[] h) {
+    buildHeap(h);
+    Comparable ret[] = new Comparable[h.length];
 
+    for (int i = 0; i < h.length; i++) {
+      ret[i] = remove(h);
+    }
 
-
-
-
-
-
-
-
+    return ret;
+  }
 }
--- HeapLab/Main.java
+++ HeapLab/Main.java
@@ -5,22 +5,35 @@ import java.util.*;
  * @version 12022024
  */
 public class Main {
-  private static int heapSize = 5;
-  private static HeapUtil hp = new HeapUtil(heapSize);
-  private static Random random = new Random();
   /**
    * main method
    * @param args cmd-line args
    */
   public static void main(String[] args) {
-    Integer[] ha = new Integer[heapSize + 1];
-    for (int i = 1; i < heapSize + 1; i++) {
-      //ha[i] = random.nextInt(100); 
-      ha[i] = 11*(heapSize - i + 1);
+    //private static int heapSize = 5;
+    Integer[] ha = new Integer[12];
+    Random random = new Random();
+    for (int i = 1; i < 12; i++) {
+      ha[i] = random.nextInt(100); 
     }
-    HeapDisplay d = new HeapDisplay();
+    HeapDisplay d1 = new HeapDisplay();
+    HeapDisplay d2 = new HeapDisplay();
+    HeapDisplay d3 = new HeapDisplay();
+    HeapUtil hp = new HeapUtil(11);
 
-    hp.heapify(ha, 1);
-    d.displayHeap((Comparable[]) ha, heapSize);
+    hp.buildHeap(ha);
+    d1.displayHeap(ha, 11);
+    hp.remove(ha);
+    d2.displayHeap(ha, 10);
+    hp.insert(ha, 22);
+    d3.displayHeap(ha, 11);
+    for (Comparable i: ha) {
+      System.out.print(i + " ");
+    }
+    System.out.println("\n-------\n");
+    Comparable sh[] = hp.heapSort(ha);
+    for (Comparable i: sh) {
+      System.out.print(i + " ");
+    }
   }
 }
