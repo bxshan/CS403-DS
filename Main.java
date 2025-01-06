379c18bafd573c6fdea58f7c51f22b74d5852ba8
Boxuan Shan <boxuan.shan@gmail.com>
2025-01-06 10:31:32 -0800

    useless

--- Main.java
+++ Main.java
@@ -14,12 +14,12 @@ import java.util.Iterator;
 //}
 
 
-class Test<E> {
-  // An object of type E is declared
-  E obj;
-  Test(E obj) { this.obj = obj; } // constructor
-  public E getObject() { return this.obj; }
-}
+//class Test<E> {
+//  // An object of type E is declared
+//  E obj;
+//  Test(E obj) { this.obj = obj; } // constructor
+//  public E getObject() { return this.obj; }
+//}
 
 
 //// Interface
@@ -41,27 +41,53 @@ class Test<E> {
 //}
 //
 
-abstract class Animal {
-  public abstract void sound();
-  public abstract void sleep();
-  public void eat() {
-    System.out.println("eat");
-  }
-} 
-
-class Dog extends Animal {
-  public void sound() {
-    System.out.println("woof");
-  }
-
-  public void sleep() {
-    System.out.println("zzzdog");
-  }
-}
+//abstract class Animal {
+//  public abstract void sound();
+//  public abstract void sleep();
+//  public void eat() {
+//    System.out.println("eat");
+//  }
+//} 
+//
+//class Dog extends Animal {
+//  public void sound() {
+//    System.out.println("woof");
+//  }
+//
+//  public void sleep() {
+//    System.out.println("zzzdog");
+//  }
+//}
 
 
 
 public class Main {
+  public static void lS(String str)
+  {
+    String s = str.substring(0, 1);
+    int c = 1;
+    int cmax = Integer.MIN_VALUE;
+    String smax = s;
+
+    if (str.length() == 0) System.out.println(s + " " + c);
+
+    for (int i = 1; i < str.length(); i++) {
+      if (str.substring(i, i+1).equals(s)) {
+        c++;
+      } else {
+        if (c > cmax) {
+          smax = s;
+          cmax = c;
+        } else {
+          cmax = Math.max(cmax, c);
+        }
+        c = 1;
+        s = str.substring(i, i+1);
+      }
+    }
+
+    System.out.println(smax + " " + cmax);
+  }
   public static void main(String[] args) {
     //OuterClass.InnerClass I = new OuterClass.InnerClass();
     //I.iprint();
@@ -76,12 +102,13 @@ public class Main {
 //    System.out.println(it.next());
 //
 
-    Dog d = new Dog();  // Create a Pig object
-    d.sound();
-    d.sleep();
-    d.eat();
+//    Dog d = new Dog();  // Create a Pig object
+//    d.sound();
+//    d.sleep();
+//    d.eat();
 
 
+    lS("aaabbbbbbbcc");
 
 
   }
