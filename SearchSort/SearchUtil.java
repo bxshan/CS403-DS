70d3b8c4e60469a1f47ad9aebbe143f86eddb058
Boxuan Shan <boxuan.shan@gmail.com>
2025-01-28 13:10:39 -0800

    +search methods

---
+++ SearchSort/SearchUtil.java
@@ -0,0 +1,49 @@
+/**
+ * SearchUtil.java
+ *
+ * @author Boxuan Shan
+ * @version 01282025
+ */
+public class SearchUtil {
+  /**
+   * performs a linear search
+   * @param a array to search in
+   * @param x element to find
+   * @return index of element x
+   */
+  public static int linearSearch(int[] a, int x) {
+    for(int i = 0; i < a.length; i++) {
+      if (a[i] == x) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  /**
+   * performs a binary search
+   * @param a array to search in
+   * @param x element to find
+   * @return index of element x
+   */
+  public static int binarySearch(int[] a, int x) {
+    int l = 0;
+    int r = a.length;
+
+    while (r-l > 1) {
+      int m = (l+r)/2;
+      if (x < a[m]) {
+        r = m;
+      } else {
+        l = m;
+      }
+    }
+
+    if (a[l] == x) {
+      return l;
+    } else {
+      return -1;
+    }
+  }
+}
---
+++ SearchSort/SearchUtilTester.java
@@ -0,0 +1,31 @@
+/**
+ * SearchUtilTester.java
+ *
+ * @author Boxuan Shan
+ * @version 01282025
+ */
+public class SearchUtilTester {
+  /**
+   * main method tester
+   * @param args cmd-line args
+   */
+  public static void main(String[] args) {
+    SearchUtil s = new SearchUtil();
+    int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    System.out.print("given a: ");
+    for(int i : a) {
+      System.out.print(i + " ");
+    }
+    
+    System.out.print("\n\ntesting linear sort\n");
+    for(int i = 0; i < a.length; i++) {
+      System.out.print("found " + i + " @ " + SearchUtil.linearSearch(a, i) + "\n");
+    }
+
+    System.out.print("\ntesting binary sort\n");
+    for(int i = 0; i < a.length; i++) {
+      System.out.print("found " + i + " @ " + SearchUtil.binarySearch(a, i) + "\n");
+    }
+  }
+}
